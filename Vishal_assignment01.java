a347b3833e44195adcd793d4cebab6a5740de18d
Vishal Rajendra Chaudhari <88141835+vc30545@users.noreply.github.com>
2022-01-13 20:19:06 +0530

    Add files via upload

---
+++ Vishal_assignment01.java
@@ -0,0 +1,30 @@
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Vishal_assignment01 {
+    public static void ShowDetails(String name) {
+
+        Random random = new Random();
+        int id = random.nextInt(100000);
+
+        String timeStamp = new SimpleDateFormat("yyyy:MM:dd  HH:mm:ss").format(Calendar.getInstance().getTime());
+
+        System.out.println("************** Details ******************");
+        System.out.println("Id          :   " + id);
+        System.out.println("Name        :   " + name);
+        System.out.println("Date & Time :   " + timeStamp);
+
+    }
+
+    public static void main(String args[]) {
+        System.out.print("Enter the name : ");
+
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        sc.close();
+
+        ShowDetails(name);
+    }
+}
